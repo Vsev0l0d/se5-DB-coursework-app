7778913bd077cb12f1ae43a433726f38fff62f0f
Vsev0l0d <vsevolod628@gmail.com>
2021-12-01 22:11:44 +0300

    Initial commit

---
+++ src/main/java/itmo/se5dbcurseworkapp/Se5DbCurseworkAppApplication.java
@@ -0,0 +1,13 @@
+package itmo.se5dbcurseworkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se5DbCurseworkAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se5DbCurseworkAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itmo/se5dbcurseworkapp/Se5DbCurseworkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.se5dbcurseworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se5DbCurseworkAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
