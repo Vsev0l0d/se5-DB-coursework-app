cef01e74f91bce4aaa458ccb8c54165f56f39b21
Vsev0l0d <vsevolod628@gmail.com>
2021-12-10 02:31:51 +0300

    index.html = Whitelabel Error Page error

--- src/main/java/itmo/controller/FrontAppController.java
+++ src/main/java/itmo/controller/FrontAppController.java
@@ -1,11 +1,12 @@
 package itmo.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FrontAppController {
-    @RequestMapping("/")
+public class FrontAppController implements ErrorController {
+    @RequestMapping(value = {"/", "/error"})
     public String getIndex() {
         return "index.html";
     }
