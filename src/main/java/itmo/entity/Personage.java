80b7ff286e0a9f0e5750d1977a984d15c05d3e41
Vsev0l0d <vsevolod628@gmail.com>
2021-12-10 01:57:52 +0300

    statistics

--- src/main/java/itmo/entity/Personage.java
+++ src/main/java/itmo/entity/Personage.java
@@ -3,7 +3,6 @@ package itmo.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.Collection;
 
 @Data
 @Entity
--- src/main/java/itmo/repository/PersonageRepository.java
+++ src/main/java/itmo/repository/PersonageRepository.java
@@ -1,7 +1,13 @@
 package itmo.repository;
 
 import itmo.entity.Personage;
+import net.minidev.json.JSONObject;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonageRepository extends CrudRepository<Personage, Integer> {
+    @Query(nativeQuery = true, value = "select weapon_count(:id), clothing_count(:id)," +
+            " prise_count(:id), share_of_accepted_invitations(:id)")
+    JSONObject statistics(@Param("id") int id);
 }
