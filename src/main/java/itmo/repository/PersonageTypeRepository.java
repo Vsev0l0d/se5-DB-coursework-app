356cd213d5d3c52050526594cf63359d213c9b1b
Vsev0l0d <vsevolod628@gmail.com>
2022-01-19 14:48:48 +0300

    PersonageTypeRepository

---
+++ src/main/java/itmo/repository/PersonageTypeRepository.java
@@ -0,0 +1,7 @@
+package itmo.repository;
+
+import itmo.entity.PersonageType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonageTypeRepository extends CrudRepository<PersonageType, Integer> {
+}
